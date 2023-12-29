c33f1ac3cd1f9f965c12907964d65c36cfef780c
ajieh <jaymycn@gmail.com>
2023-12-29 22:05:58 +0800

    中值滤波

---
+++ chapter5/MedianFilterExample.cpp
@@ -0,0 +1,69 @@
+#include <vtkImageActor.h>
+#include <vtkImageCast.h>
+#include <vtkInteractorStyleImage.h>
+#include <vtkImageAccumulate.h>
+#include <vtkImageData.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkSmartPointer.h>
+#include <vtkJPEGReader.h>
+#include <vtkImageHybridMedian2D.h>
+#include <vtkImageThreshold.h>
+#include <vtkAutoInit.h>
+
+VTK_MODULE_INIT(vtkRenderingOpenGL2)
+VTK_MODULE_INIT(vtkRenderingFreeType)
+VTK_MODULE_INIT(vtkInteractionStyle)
+
+int main()
+{
+	vtkSmartPointer<vtkJPEGReader> reader = vtkSmartPointer<vtkJPEGReader>::New();
+	reader->SetFileName("D:\\Documents\\Material\\VTK-learn\\Examples\\Chap05\\DATA\\lena-gray.jpg");
+	reader->Update();
+
+	//中值滤波
+	vtkSmartPointer<vtkImageHybridMedian2D> medianFilter =
+		vtkSmartPointer<vtkImageHybridMedian2D>::New();
+	medianFilter->SetInputData(reader->GetOutput());
+	medianFilter->Update();
+
+	vtkSmartPointer<vtkImageActor> originalActor = vtkSmartPointer<vtkImageActor>::New();
+	originalActor->SetInputData(reader->GetOutput());
+
+	vtkSmartPointer<vtkImageActor> outputActor = vtkSmartPointer<vtkImageActor>::New();
+	outputActor->SetInputData(medianFilter->GetOutput());
+
+	vtkSmartPointer<vtkRenderer> originalRenderer = vtkSmartPointer<vtkRenderer>::New();
+	originalRenderer->SetViewport(0.0, 0.0, 0.5, 1.0);
+	originalRenderer->AddActor(originalActor);
+	originalRenderer->ResetCamera();
+	originalRenderer->SetBackground(1.0, 1.0, 1.0);
+
+	vtkSmartPointer<vtkRenderer> outputRenderer = vtkSmartPointer<vtkRenderer>::New();
+	outputRenderer->SetViewport(0.5, 0.0, 1.0, 1.0);
+	outputRenderer->AddActor(outputActor);
+	outputRenderer->ResetCamera();
+	outputRenderer->SetBackground(1.0, 1.0, 1.0);
+
+	vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+	renderWindow->AddRenderer(originalRenderer);
+	renderWindow->AddRenderer(outputRenderer);
+	renderWindow->SetSize(600, 300);
+	renderWindow->Render();
+	renderWindow->SetWindowName("MedianFilterExample");
+
+	vtkSmartPointer<vtkRenderWindowInteractor> interactor =
+		vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	vtkSmartPointer<vtkInteractorStyleImage> style =
+		vtkSmartPointer<vtkInteractorStyleImage>::New();
+
+	interactor->SetInteractorStyle(style);
+	interactor->SetRenderWindow(renderWindow);
+
+	interactor->Initialize();
+	interactor->Start();
+	return 0;
+}
+
+
