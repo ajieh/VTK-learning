6e42fd469dbffded2f15422dde754fa736b83902
ajieh <jaymycn@gmail.com>
2023-12-29 22:02:33 +0800

    高斯平滑滤波

---
+++ chapter5/GaussianFilterExample.cpp
@@ -0,0 +1,73 @@
+#include <vtkSmartPointer.h>
+#include <vtkImageData.h>
+#include <vtkImageGaussianSmooth.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkInteractorStyleImage.h>
+#include <vtkRenderer.h>
+#include <vtkImageActor.h>
+#include <vtkImageEllipsoidSource.h>
+#include <vtkImageCast.h>
+#include <vtkJPEGReader.h>
+#include <vtkAutoInit.h>
+
+VTK_MODULE_INIT(vtkRenderingOpenGL2)
+VTK_MODULE_INIT(vtkRenderingFreeType)
+VTK_MODULE_INIT(vtkInteractionStyle)
+
+int main()
+{
+	vtkSmartPointer<vtkJPEGReader> reader = vtkSmartPointer<vtkJPEGReader>::New();
+	reader->SetFileName("D:\\Documents\\Material\\VTK-learn\\Examples\\Chap05\\DATA\\lena-gray.jpg");
+	reader->Update();
+
+	//高斯平滑滤波
+	vtkSmartPointer<vtkImageGaussianSmooth> gaussianSmoothFilter =
+		vtkSmartPointer<vtkImageGaussianSmooth>::New();
+	gaussianSmoothFilter->SetInputConnection(reader->GetOutputPort());
+	gaussianSmoothFilter->SetDimensionality(2);
+	gaussianSmoothFilter->SetRadiusFactor(5);//高斯模板大小
+	gaussianSmoothFilter->SetStandardDeviation(3);//标准差
+	gaussianSmoothFilter->Update();
+
+	vtkSmartPointer<vtkImageActor> originalActor =
+		vtkSmartPointer<vtkImageActor>::New();
+	originalActor->SetInputData(reader->GetOutput());
+
+	vtkSmartPointer<vtkImageActor> outputActor =
+		vtkSmartPointer<vtkImageActor>::New();
+	outputActor->SetInputData(gaussianSmoothFilter->GetOutput());
+
+	vtkSmartPointer<vtkRenderer> originalRenderer = vtkSmartPointer<vtkRenderer>::New();
+	originalRenderer->SetViewport(0.0, 0.0, 0.5, 1.0);
+	originalRenderer->AddActor(originalActor);
+	originalRenderer->ResetCamera();
+	originalRenderer->SetBackground(1.0, 1.0, 1.0);
+
+	vtkSmartPointer<vtkRenderer> outputRenderer = vtkSmartPointer<vtkRenderer>::New();
+	outputRenderer->SetViewport(0.5, 0.0, 1.0, 1.0);
+	outputRenderer->AddActor(outputActor);
+	outputRenderer->ResetCamera();
+	outputRenderer->SetBackground(1.0, 1.0, 1.0);
+
+	vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+	renderWindow->AddRenderer(originalRenderer);
+	renderWindow->AddRenderer(outputRenderer);
+	renderWindow->SetSize(600, 300);
+	renderWindow->Render();
+	renderWindow->SetWindowName("GaussianFilterExample");
+
+	vtkSmartPointer<vtkRenderWindowInteractor> interactor =
+		vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	vtkSmartPointer<vtkInteractorStyleImage> style =
+		vtkSmartPointer<vtkInteractorStyleImage>::New();
+
+	interactor->SetInteractorStyle(style);
+	interactor->SetRenderWindow(renderWindow);
+
+	interactor->Initialize();
+	interactor->Start();
+	return 0;
+}
+
+
