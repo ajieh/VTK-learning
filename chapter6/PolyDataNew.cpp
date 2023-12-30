02b283ed013374891895f5900ef917f3707bf076
ajieh <jaymycn@gmail.com>
2023-12-30 14:30:45 +0800

    PolyData的创建

---
+++ chapter6/PolyDataNew.cpp
@@ -0,0 +1,75 @@
+#include <vtkSmartPointer.h>
+#include <vtkPolygon.h>
+#include <vtkTriangle.h>
+#include <vtkCellArray.h>
+#include <vtkPolyData.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkProperty.h>
+#include <iostream>
+#include <vtkAutoInit.h>
+
+VTK_MODULE_INIT(vtkRenderingOpenGL2)
+VTK_MODULE_INIT(vtkRenderingFreeType)
+VTK_MODULE_INIT(vtkInteractionStyle)
+
+int main()
+{
+	//存储点集合
+	vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
+	points->InsertNextPoint(0.0, 0.0, 0.0);
+	points->InsertNextPoint(1.0, 0.0, 0.0);
+	points->InsertNextPoint(1.0, 1.0, 0.0);
+	points->InsertNextPoint(0.0, 1.0, 0.0);
+	points->InsertNextPoint(2.0, 0.0, 0.0);
+
+	//获取多边形的点索引对象
+	vtkSmartPointer<vtkPolygon> polygon = vtkSmartPointer<vtkPolygon>::New();
+	polygon->GetPointIds()->SetNumberOfIds(4);
+	polygon->GetPointIds()->SetId(0, 0);
+	polygon->GetPointIds()->SetId(1, 1);
+	polygon->GetPointIds()->SetId(2, 2);
+	polygon->GetPointIds()->SetId(3, 3);
+
+	vtkSmartPointer<vtkTriangle> trianle = vtkSmartPointer<vtkTriangle>::New();
+	trianle->GetPointIds()->SetId(0, 1);
+	trianle->GetPointIds()->SetId(1, 2);
+	trianle->GetPointIds()->SetId(2, 4);
+
+	vtkSmartPointer<vtkCellArray> cells = vtkSmartPointer<vtkCellArray>::New();
+	cells->InsertNextCell(polygon);
+	cells->InsertNextCell(trianle);
+
+	vtkSmartPointer<vtkPolyData> polygonPolyData = vtkSmartPointer<vtkPolyData>::New();
+	polygonPolyData->SetPoints(points);
+	polygonPolyData->SetPolys(cells);
+
+	vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+	mapper->SetInputData(polygonPolyData);
+
+	vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
+	actor->SetMapper(mapper);
+
+	vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
+	renderer->AddActor(actor);
+
+	vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+	renderWindow->AddRenderer(renderer);
+	renderWindow->SetSize(600, 400);
+	renderWindow->Render();
+	renderWindow->SetWindowName("PolyDataNew");
+
+	vtkSmartPointer<vtkRenderWindowInteractor> interactor =
+		vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	interactor->SetRenderWindow(renderWindow);
+	interactor->Initialize();
+	interactor->Start();
+	return 0;
+}
+
+
+
+
