c870a88db7790957fa66fdb4320757401d1b7a36
ajieh <jaymycn@gmail.com>
2023-12-31 20:14:34 +0800

    PolyDataLapLasianSmooth.cpp

---
+++ chapter6/PolyDataLapLasianSmooth.cpp
@@ -0,0 +1,74 @@
+#include <vtkSmartPointer.h>
+#include <vtkPolyDataReader.h>
+#include <vtkPolyData.h>
+#include <vtkSmoothPolyDataFilter.h>
+#include <vtkWindowedSincPolyDataFilter.h>
+#include <vtkSphereSource.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkAutoInit.h>
+
+VTK_MODULE_INIT(vtkRenderingOpenGL2)
+VTK_MODULE_INIT(vtkRenderingFreeType)
+VTK_MODULE_INIT(vtkInteractionStyle)
+
+int main()
+{
+	vtkSmartPointer<vtkPolyDataReader> reader = vtkSmartPointer<vtkPolyDataReader>::New();
+	reader->SetFileName("D:\\Documents\\Material\\VTK-learn\\Examples\\Chap06\\DATA\\FRAN_CUT.VTK");
+	reader->Update();
+
+	//网格平滑
+	vtkSmartPointer<vtkSmoothPolyDataFilter> smoothFilter =
+		vtkSmartPointer<vtkSmoothPolyDataFilter>::New();
+	smoothFilter->SetInputConnection(reader->GetOutputPort());
+	smoothFilter->SetNumberOfIterations(100);//平滑次数
+	smoothFilter->Update();
+
+	vtkSmartPointer<vtkPolyDataMapper> originalMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+	originalMapper->SetInputConnection(reader->GetOutputPort());
+
+	vtkSmartPointer<vtkActor> originalActor = vtkSmartPointer<vtkActor>::New();
+	originalActor->SetMapper(originalMapper);
+
+	vtkSmartPointer<vtkPolyDataMapper> smoothedMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+	smoothedMapper->SetInputConnection(smoothFilter->GetOutputPort());
+
+	vtkSmartPointer<vtkActor> smoothedActor = vtkSmartPointer<vtkActor>::New();
+	smoothedActor->SetMapper(smoothedMapper);
+	
+
+	vtkSmartPointer<vtkRenderer> renderer1 = vtkSmartPointer<vtkRenderer>::New();
+	renderer1->SetViewport(0.0, 0.0, 0.5, 1.0);
+	renderer1->AddActor(originalActor);
+	renderer1->ResetCamera();
+	renderer1->SetBackground(1.0, 1.0, 1.0);
+
+	vtkSmartPointer<vtkRenderer> renderer2 = vtkSmartPointer<vtkRenderer>::New();
+	renderer2->SetViewport(0.5, 0.0, 1.0, 1.0);
+	renderer2->AddActor(smoothedActor);
+	renderer2->ResetCamera();
+	renderer2->SetBackground(1.0, 1.0, 1.0);
+
+
+	vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+	renderWindow->AddRenderer(renderer1);
+	renderWindow->AddRenderer(renderer2);
+	renderWindow->SetSize(600, 400);
+	renderWindow->Render();
+	renderWindow->SetWindowName("PolyDataLapLasianSmooth");
+
+	vtkSmartPointer<vtkRenderWindowInteractor> interactor =
+		vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	interactor->SetRenderWindow(renderWindow);
+	interactor->Initialize();
+	interactor->Start();
+	return 0;
+}
+
+
+
+
