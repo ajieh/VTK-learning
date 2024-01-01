269232921e1e80dd912621b95e03a3fb7572aa09
ajieh <jaymycn@gmail.com>
2024-01-01 15:13:40 +0800

    巴特沃斯低通滤波器

--- chapter5/ButterworthLowPassExample.cpp
+++ chapter5/ButterworthLowPassExample.cpp
@@ -1,53 +1,41 @@
-/**********************************************************************
-
-  文件名: 5.6_ButterworthLowPassExample.cpp
-  Copyright (c) 张晓东, 罗火灵. All rights reserved.
-  更多信息请访问: 
-    http://www.vtkchina.org (VTK中国)
-	http://blog.csdn.net/www_doling_net (东灵工作室) 
-
-**********************************************************************/
-
 #include <vtkSmartPointer.h>
 #include <vtkImageData.h>
 #include <vtkImageButterworthLowPass.h>
 #include <vtkImageActor.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
+#include <vtkInteractorStyleImage.h>
 #include <vtkRenderer.h>
 #include <vtkJPEGReader.h>
 #include <vtkImageFFT.h>
 #include <vtkImageRFFT.h>
 #include <vtkImageExtractComponents.h>
 #include <vtkImageCast.h>
+#include <vtkAutoInit.h>
 
-//测试图像：../data/lena-gray.jpg
-int main(int argc, char* argv[])
+VTK_MODULE_INIT(vtkRenderingOpenGL2)
+VTK_MODULE_INIT(vtkRenderingFreeType)
+VTK_MODULE_INIT(vtkInteractionStyle)
+
+int main()
 {
-	if (argc < 2)
-	{
-		std::cout<<argv[0]<<" "<<"ImageFile(*.jpg)"<<std::endl;
-		return EXIT_FAILURE;
-	}
-	vtkSmartPointer<vtkJPEGReader> reader = 
-		vtkSmartPointer<vtkJPEGReader>::New();
-	reader->SetFileName(argv[1]);
+	vtkSmartPointer<vtkJPEGReader> reader = vtkSmartPointer<vtkJPEGReader>::New();
+	reader->SetFileName("D:\\Documents\\Material\\VTK-learn\\Examples\\Chap05\\DATA\\lena-gray.jpg");
 	reader->Update();
 
-	vtkSmartPointer<vtkImageFFT> fftFilter =
-		vtkSmartPointer<vtkImageFFT>::New();
+	vtkSmartPointer<vtkImageFFT> fftFilter = vtkSmartPointer<vtkImageFFT>::New();
 	fftFilter->SetInputConnection(reader->GetOutputPort());
 	fftFilter->Update();
 
-	vtkSmartPointer<vtkImageButterworthLowPass> lowPassFilter = 
+	//宸寸壒娌冩柉浣庨�氭护娉㈠櫒
+	vtkSmartPointer<vtkImageButterworthLowPass> lowPassFilter =
 		vtkSmartPointer<vtkImageButterworthLowPass>::New();
 	lowPassFilter->SetInputConnection(fftFilter->GetOutputPort());
 	lowPassFilter->SetXCutOff(0.05);
 	lowPassFilter->SetYCutOff(0.05);
 	lowPassFilter->Update();
 
-	vtkSmartPointer<vtkImageRFFT> rfftFilter =
-		vtkSmartPointer<vtkImageRFFT>::New();
+	vtkSmartPointer<vtkImageRFFT> rfftFilter = vtkSmartPointer<vtkImageRFFT>::New();
 	rfftFilter->SetInputConnection(lowPassFilter->GetOutputPort());
 	rfftFilter->Update();
 
@@ -56,53 +44,47 @@ int main(int argc, char* argv[])
 	ifftExtractReal->SetInputConnection(rfftFilter->GetOutputPort());
 	ifftExtractReal->SetComponents(0);
 
-	vtkSmartPointer<vtkImageCast> castFilter =
-		vtkSmartPointer<vtkImageCast>::New();
+	vtkSmartPointer<vtkImageCast> castFilter = vtkSmartPointer<vtkImageCast>::New();
 	castFilter->SetInputConnection(ifftExtractReal->GetOutputPort());
 	castFilter->SetOutputScalarTypeToUnsignedChar();
 	castFilter->Update();
 
 	vtkSmartPointer<vtkImageActor> originalActor =
 		vtkSmartPointer<vtkImageActor>::New();
-	originalActor->SetInput(reader->GetOutput());
+	originalActor->SetInputData(reader->GetOutput());
 
-	vtkSmartPointer<vtkImageActor> erodedActor =
-		vtkSmartPointer<vtkImageActor>::New();
-	erodedActor->SetInput(castFilter->GetOutput());
+	vtkSmartPointer<vtkImageActor> outputActor = vtkSmartPointer<vtkImageActor>::New();
+	outputActor->SetInputData(castFilter->GetOutput());
 
-	double leftViewport[4] = {0.0, 0.0, 0.5, 1.0};
-	double rightViewport[4] = {0.5, 0.0, 1.0, 1.0};
+	vtkSmartPointer<vtkRenderer> originalRenderer = vtkSmartPointer<vtkRenderer>::New();
+	originalRenderer->SetViewport(0.0, 0.0, 0.5, 1.0);
+	originalRenderer->AddActor(originalActor);
+	originalRenderer->ResetCamera();
+	originalRenderer->SetBackground(1.0, 1.0, 1.0);
 
-	vtkSmartPointer<vtkRenderWindow> renderWindow =
-		vtkSmartPointer<vtkRenderWindow>::New();
-	renderWindow->SetSize(640, 320);
+	vtkSmartPointer<vtkRenderer> outputRenderer = vtkSmartPointer<vtkRenderer>::New();
+	outputRenderer->SetViewport(0.5, 0.0, 1.0, 1.0);
+	outputRenderer->AddActor(outputActor);
+	outputRenderer->ResetCamera();
+	outputRenderer->SetBackground(1.0, 1.0, 1.0);
+
+	vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+	renderWindow->AddRenderer(originalRenderer);
+	renderWindow->AddRenderer(outputRenderer);
+	renderWindow->SetSize(600, 300);
 	renderWindow->Render();
 	renderWindow->SetWindowName("ButterworthLowPassExample");
 
 	vtkSmartPointer<vtkRenderWindowInteractor> interactor =
 		vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	vtkSmartPointer<vtkInteractorStyleImage> style =
+		vtkSmartPointer<vtkInteractorStyleImage>::New();
+
+	interactor->SetInteractorStyle(style);
 	interactor->SetRenderWindow(renderWindow);
 
-	vtkSmartPointer<vtkRenderer> leftRenderer =
-		vtkSmartPointer<vtkRenderer>::New();
-	renderWindow->AddRenderer(leftRenderer);
-	leftRenderer->SetViewport(leftViewport);
-	leftRenderer->SetBackground(1.0, 1.0, 1.0);
-
-	vtkSmartPointer<vtkRenderer> rightRenderer =
-		vtkSmartPointer<vtkRenderer>::New();
-	renderWindow->AddRenderer(rightRenderer);
-	rightRenderer->SetViewport(rightViewport);
-	rightRenderer->SetBackground(1.0, 1.0, 1.0);
-
-	leftRenderer->AddActor(originalActor);
-	rightRenderer->AddActor(erodedActor);
-
-	leftRenderer->ResetCamera();
-	rightRenderer->ResetCamera();
-
-	renderWindow->Render();
+	interactor->Initialize();
 	interactor->Start();
 
-	return EXIT_SUCCESS;
+	return 0;
 }
